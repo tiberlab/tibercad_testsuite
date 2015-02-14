0042aa2d694b3776343b01eb8222358743526005
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2015-02-14 15:13:25 +0000

    * TestSuite.pm: changed absolute tolerance to fixed 1e-12, instead of having   it depend on the relative tolerance provided from input

--- TestSuite.pm
+++ TestSuite.pm
@@ -757,6 +757,7 @@ sub compare_data_vectors($$$) {
   my @data1 = @{$_[0]};
   my @data2 = @{$_[1]};
   my $prec = $_[2];
+  my $eps = 1e-12;
 
   my $n = $#data1;
 
@@ -764,7 +765,7 @@ sub compare_data_vectors($$$) {
   my $difference = 0;
   for (my $i = 0; $i <= $n; $i++) {
     my $diff = $data1[$i] - $data2[$i];
-    if (abs($diff) > $prec * (1.0 + abs($data1[$i]))) {
+    if (abs($diff) > ($prec * abs($data1[$i] + $eps))) {
       #print "$data1[$i] $data2[$i] $diff\n";
       $difference = 1;
       last;
