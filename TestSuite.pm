8210c91dc247dcf045500c46b96256b95353210e
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2015-02-26 08:35:15 +0000

    * adjusted testsuite to work with new absolute precision * added some missing test configurations

--- TestSuite.pm
+++ TestSuite.pm
@@ -766,7 +766,7 @@ sub compare_data_vectors($$$) {
   for (my $i = 0; $i <= $n; $i++) {
     my $diff = $data1[$i] - $data2[$i];
     if (abs($diff) > ($prec * abs($data1[$i]) + $eps)) {
-      print "$data1[$i] $data2[$i] $diff\n";
+      #print "$data1[$i] $data2[$i] $diff\n";
       $difference = 1;
       last;
     }
